54796faf35f7a51839033bb4b5dacad83c863c95
Kimmo Hyvärinen <kimmo.hyvarinen@mediasignal.fi>
2021-09-09 11:48:07 +0300

    Fix

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+
+// serve the public directory as a static content here
+app.use(express.static("public"));
+
 const server = app.listen(port, () => {
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
 });
 
-// serve the public directory as a static content here
-app.use(express.static("public"));
+
