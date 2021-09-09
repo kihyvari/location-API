5c45fffce57f839e46e3b391da0e4f30ca70adaf
Kimmo Hyvärinen <kimmo.hyvarinen@mediasignal.fi>
2021-09-09 13:32:11 +0300

    Add location api

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 // serve the public directory as a static content here
+
+
+let database = [
+  { id: 1, latitude: 60, longitude: 70 },
+  { id: 2, latitude: 40, longitude: 80 },
+];
+
+const str = JSON.stringify(database, null, 2);
+
+app.get("/api/locations", function (req, res) {
+  res.send(str);
+});
+
 app.use(express.static("public"));
 
 app.listen(port, () => {
