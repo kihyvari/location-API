fae5be47e9a58f4ffc61fa0f44f212e227d7f7c7
Kimmo Hyvärinen <kimmo.hyvarinen@mediasignal.fi>
2021-09-09 11:52:47 +0300

    Fix again

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
 // serve the public directory as a static content here
 app.use(express.static("public"));
 
-const server = app.listen(port, () => {
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
