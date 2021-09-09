7e33d7fc22f850464a7e053fc5ae4f69c1e6fc72
Kimmo Hyvärinen <kimmo.hyvarinen@mediasignal.fi>
2021-09-09 11:21:29 +0300

    Add Heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => { .. })
 
 // serve the public directory as a static content here
 app.use(express.static('public'))
