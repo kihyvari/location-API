f7239cab9e28681764d1eebe301f80d4a45160d9
Kimmo Hyvärinen <kimmo.hyvarinen@mediasignal.fi>
2021-09-09 11:28:34 +0300

    Fix error

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => { .. })
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+});
 
 // serve the public directory as a static content here
-app.use(express.static('public'))
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.use(express.static("public"));
